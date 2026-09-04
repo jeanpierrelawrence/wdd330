37c5a5a17740fa533fc495e34773d08d09cb9bb7
Jp Lawrence <jeannpierrelawrence@gmail.com>
2026-09-04 20:44:21 +0200

    working cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,15 +1,14 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
+  const cart = getLocalStorage("so-cart") || [];
 
-	const cart = getLocalStorage("so-cart") || [];
+  cart.push(product);
 
-	cart.push(product);
-
-  	setLocalStorage("so-cart", cart);
+  setLocalStorage("so-cart", cart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
