1fd8f778719093537c01666b4e9786696d58da5f
Jp Lawrence <jeannpierrelawrence@gmail.com>
2026-09-04 20:32:45 +0200

    baseline changes

--- src/js/product.js
+++ src/js/product.js
@@ -4,7 +4,12 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+
+	const cart = getLocalStorage("so-cart") || [];
+
+	cart.push(product);
+
+  	setLocalStorage("so-cart", cart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
